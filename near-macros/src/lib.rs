9ea344f27cd1cac97ea7b9d7112b4079ae809e2d
Buckram123 <m.donchenko@4irelabs.com>
2022-03-21 19:41:15 +0200

    display_metadata fn

--- near-macros/src/lib.rs
+++ near-macros/src/lib.rs
@@ -3,13 +3,17 @@ extern crate proc_macro;
 
 mod core_impl;
 
+use std::io::Read;
+
 use proc_macro2::TokenStream;
+use walkdir::WalkDir;
 
 use self::core_impl::*;
 use proc_macro2::Span;
 use quote::quote;
 use syn::visit::Visit;
 use syn::{File, ItemEnum, ItemImpl, ItemStruct, ItemTrait};
+use syn::__private::ToTokens;
 
 /// `metadata` generates the metadata method and should be placed at the very end of the `lib.rs` file.
 // TODO: Once Rust allows inner attributes and custom procedural macros for modules we should switch this
@@ -32,5 +36,18 @@ pub fn metadata(item: proc_macro2::TokenStream) -> TokenStream {
                 "Failed to parse code decorated with `metadata!{}` macro. Only valid Rust is supported.",
             )
             .to_compile_error()
+    }
+}
+
+pub fn display_metadata() {
+    for entry in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
+        if entry.path().extension().map(|s| s == "rs").unwrap_or(false) {
+            let mut file = std::fs::File::open(&entry.path()).expect("Unable to open file");
+            let mut src = String::new();
+            file.read_to_string(&mut src).expect("Unable to read file");
+            let syntax = syn::parse_file(&src).expect("Unable to parse file");
+            let tokens = metadata(syntax.to_token_stream());
+            println!("\n{}:\n{}", entry.path().display(), tokens);
+        }
     }
 }
--- playing-with-syn/src/main.rs
+++ playing-with-syn/src/main.rs
@@ -4,15 +4,15 @@ use std::io::Read;
 use syn::__private::ToTokens;
 use walkdir::WalkDir;
 
-fn main() {
+pub fn main() {
     for entry in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
         if entry.path().extension().map(|s| s == "rs").unwrap_or(false) {
             let mut file = File::open(&entry.path()).expect("Unable to open file");
             let mut src = String::new();
             file.read_to_string(&mut src).expect("Unable to read file");
             let syntax = syn::parse_file(&src).expect("Unable to parse file");
-            let a = near_macros::metadata(syntax.to_token_stream());
-            println!("{}", a);
+            let tokens = near_macros::metadata(syntax.to_token_stream());
+            println!("\n{}:\n{}", entry.path().display(), tokens);
         }
     }
 }
