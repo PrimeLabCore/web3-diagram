d5c8bc3fa6013c0d07d2c1cec8d632f337f45255
Deveus <raffree.dev@gmail.com>
2022-03-23 02:07:33 +0200

    Minor change

--- example/src/main.rs
+++ example/src/main.rs
@@ -3,9 +3,6 @@ mod lib;
 mod some_fancy_impl;
 use scanner_syn::contract_descriptor::{DefaultContractDescriptor, ContractDescriptor};
 fn main() {
-   // scanner_syn::display_metadata();
-
-
     let desc=DefaultContractDescriptor::new();
     desc.get_contract_info_for_crate();
 }
